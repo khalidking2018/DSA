d2d1126db923493e423c5b7f4d0d16483e9dfe12
khalidking2018 <134798603+khalidking2018@users.noreply.github.com>
2025-08-03 15:17:53 +0530

    Time: 3 ms (36.42%) | Memory: 10.5 MB (67.18%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,57 @@
-class Solution {
-public:
-    void getAllSubsets(vector<int>& nums,vector<int> ans,vector<vector<int>> &allSubsets,int i){
-        if(i==nums.size()){
-            allSubsets.push_back(ans);
-            return;
-        }
+// class Solution {
+// public:
+//     void getAllSubsets(vector<int>& nums,vector<int> ans,vector<vector<int>> &allSubsets,int i){
+//         if(i==nums.size()){
+//             allSubsets.push_back(ans);
+//             return;
+//         }
 
-        ans.push_back(nums[i]);
-        getAllSubsets(nums,ans,allSubsets,i+1);
+//         ans.push_back(nums[i]);
+//         getAllSubsets(nums,ans,allSubsets,i+1);
 
-        ans.pop_back();
+//         ans.pop_back();
         
-        int idx=i+1;
-        while(idx < nums.size() && nums[idx]==nums[idx-1]) idx++;
+//         int idx=i+1;
+//         while(idx < nums.size() && nums[idx]==nums[idx-1]) idx++;
 
-        getAllSubsets(nums,ans,allSubsets,idx);
-    }
+//         getAllSubsets(nums,ans,allSubsets,idx);
+//     }
 
     
 
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> allSubsets;
-        vector<int> ans;
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<vector<int>> allSubsets;
+//         vector<int> ans;
 
-        getAllSubsets(nums,ans,allSubsets,0);
-        return allSubsets;
+//         getAllSubsets(nums,ans,allSubsets,0);
+//         return allSubsets;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+
+        for (int i = start; i < nums.size(); ++i) {
+            // Skip duplicates
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            current.push_back(nums[i]);
+            backtrack(i + 1, nums, current, result);
+            current.pop_back(); // Backtrack
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());  // Important: sort to bring duplicates together
+        vector<vector<int>> result;
+        vector<int> current;
+
+        backtrack(0, nums, current, result);
+        return result;
     }
 };
