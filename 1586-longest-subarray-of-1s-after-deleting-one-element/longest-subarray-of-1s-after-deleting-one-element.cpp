29ed1b4ec7a69d6bf064249c594d4a4ca15f7e6d
khalidking2018 <134798603+khalidking2018@users.noreply.github.com>
2025-08-24 11:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 60.1 MB (59.08%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,27 +1,52 @@
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int n=nums.size();
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             int one=0;
+//             int zero=0;
+//             for(int j=i;j<n;j++)
+//             {
+//                if(nums[j]==1){
+//                 one++;
+//                }
+//                else{
+//                 zero++;
+//                }
+//                if(zero <= 1){
+//                 ans=max(one,ans);
+//                }
+//                else{
+//                 break;
+//                }
+//             }
+//         }
+//         return ans==n ? ans-1: ans;
+//     }
+// };
+
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int n=nums.size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int one=0;
-            int zero=0;
-            for(int j=i;j<n;j++)
-            {
-               if(nums[j]==1){
-                one++;
-               }
-               else{
-                zero++;
-               }
-               if(zero <= 1){
-                ans=max(one,ans);
-               }
-               else{
-                break;
-               }
+        int n = nums.size();
+        int left = 0, zeroCount = 0, ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) 
+                zeroCount++;
+
+            // Shrink window if more than 1 zero
+            while (zeroCount > 1) {
+                if (nums[left] == 0) 
+                    zeroCount--;
+                left++;
             }
+
+            // Window length is (right - left), because we must delete one element
+            ans = max(ans, right - left);
         }
-        return ans==n ? ans-1: ans;
+
+        return ans;
     }
 };
