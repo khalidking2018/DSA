4ad1477c7a67fe0346d4d6f127d435a9efeb19fa
khalidking2018 <134798603+khalidking2018@users.noreply.github.com>
2025-09-13 12:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (44.54%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,32 +1,60 @@
+// class Solution {
+// public:
+//     bool isVowel(char ch){
+//         char c=ch;
+//         return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+//     }
+//     int maxFreqSum(string s) {
+//         unordered_map<char,int> vowel;
+//         unordered_map<char,int> cons;
+//         for(char ch: s){
+//             if(isVowel(ch)){
+//            vowel[ch]++;
+//             }
+//             else{
+//                 cons[ch]++;
+//             }
+//         }
+//         int max_vowel=0;
+//         int max_cons=0;
+//         for(auto &p: vowel){
+//             if(p.second > max_vowel){
+//                 max_vowel=p.second;
+//             }
+//         }
+//         for(auto &p: cons){
+//             if(p.second > max_cons){
+//                 max_cons=p.second;
+//             }
+//         }
+//         return max_vowel+max_cons;
+//     }
+// };
+
 class Solution {
 public:
     bool isVowel(char ch){
-        char c=ch;
-        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+        return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
     }
+
     int maxFreqSum(string s) {
-        unordered_map<char,int> vowel;
-        unordered_map<char,int> cons;
+        unordered_map<char,int> freq;
+
+        // Count frequencies of all characters
         for(char ch: s){
-            if(isVowel(ch)){
-           vowel[ch]++;
-            }
-            else{
-                cons[ch]++;
-            }
+            freq[ch]++;
         }
-        int max_vowel=0;
-        int max_cons=0;
-        for(auto &p: vowel){
-            if(p.second > max_vowel){
-                max_vowel=p.second;
-            }
+
+        int max_vowel = 0, max_cons = 0;
+
+        // Traverse once and separate max values
+        for (auto &p : freq) {
+            if (isVowel(p.first)) 
+                max_vowel = max(max_vowel, p.second);
+            else 
+                max_cons = max(max_cons, p.second);
         }
-        for(auto &p: cons){
-            if(p.second > max_cons){
-                max_cons=p.second;
-            }
-        }
-        return max_vowel+max_cons;
+
+        return max_vowel + max_cons;
     }
 };
