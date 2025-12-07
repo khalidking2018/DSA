c8d15b5e520113da722600e9f7b9f73cbd904183
khalidking2018 <134798603+khalidking2018@users.noreply.github.com>
2025-12-08 00:02:36 +0530

    Time: 4 ms (53.86%) | Memory: 14.8 MB (52.88%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int size=nums.size();
-        vector<int> ans;
-        for(long long i=0;i<size;i++)
-        {
-            for(long long j=i+1;j<size;j++)
-            {
-                if(nums[i]+nums[j]== target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        int n=nums.size();
+        
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++) {
+            numsWithIndex.push_back({nums[i], i});
+        }
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if (sum == target) {
+                // Return original indices
+                return {numsWithIndex[left].second, numsWithIndex[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return ans;
+        return {-1, -1};
     }
 };
