1b179cf9fa3c5f9d93e65baff0b1951a2eb3d93c
khalidking2018 <134798603+khalidking2018@users.noreply.github.com>
2026-01-01 16:32:12 +0530

    Time: 2 ms (43.96%) | Memory: 10.2 MB (67.07%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-    int n = s.length();
-    string ans = "";
-
-    // Reverse the whole string first
-    reverse(s.begin(), s.end());
-
-    for (int i = 0; i < n; i++) {
-        string word = "";
-
-        // Extract each word
-        while (i < n && s[i] != ' ') {
-            word += s[i];
-            i++;
+        int n =s.size();
+        string ans="";
+        reverse(s.begin(),s.end());
+        for(int i=0;i<n;i++){
+            string temp="";
+            if(i<n && s[i]==' ') i++;
+            while(i<n && s[i] != ' '){
+                temp+=s[i];
+                i++;
+            }
+            if(!temp.empty()){
+                reverse(temp.begin(),temp.end());
+                 if (!ans.empty()) ans += " ";
+                ans += temp;
+            }
         }
-
-        // Reverse the word back to original order
-        reverse(word.begin(), word.end());
-
-        if (word.length() > 0) {
-            ans += " " + word;
-        }
+        return ans;
     }
-
-    // Remove the leading space
-    return ans.substr(1);
-}
 };
